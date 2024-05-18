21bafaf964a728182bff80345fc6169b383360e2
Michael Zandona <michael.zandona@wheelease.com>
2024-05-18 10:46:27 -0700

    fixed service worker #3

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -1,12 +1,64 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth, getIdToken } from "firebase/auth";
+// import { getInstallations, getToken } from "firebase/installations";
+
+// // this is set during install
+// let firebaseConfig;
+
+// self.addEventListener("install", (event) => {
+//   // extract firebase config from query string
+//   const serializedFirebaseConfig = new URL(location).searchParams.get(
+//     "firebaseConfig"
+//   );
+
+//   if (!serializedFirebaseConfig) {
+//     throw new Error(
+//       "Firebase Config object not found in service worker query string."
+//     );
+//   }
+
+//   firebaseConfig = JSON.parse(serializedFirebaseConfig);
+//   console.log("Service worker installed with Firebase config", firebaseConfig);
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   const { origin } = new URL(event.request.url);
+//   if (origin !== self.location.origin) return;
+//   event.respondWith(fetchWithFirebaseHeaders(event.request));
+// });
+
+// // TODO: add Firebase Authentication headers to request
+// async function fetchWithFirebaseHeaders(request) {
+//   const app = initializeApp(firebaseConfig);
+//   const auth = getAuth(app);
+//   const installations = getInstallations(app);
+//   const headers = new Headers(request.headers);
+//   const [authIdToken, installationToken] = await Promise.all([
+//     getAuthIdToken(auth),
+//     getToken(installations),
+//   ]);
+//   headers.append("Firebase-Instance-ID-Token", installationToken);
+//   if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
+//   const newRequest = new Request(request, { headers });
+//   return await fetch(newRequest);
+// }
+
+// async function getAuthIdToken(auth) {
+//   await auth.authStateReady();
+//   if (!auth.currentUser) return;
+//   return await getIdToken(auth.currentUser);
+// }
+
 import { initializeApp } from "firebase/app";
 import { getAuth, getIdToken } from "firebase/auth";
 import { getInstallations, getToken } from "firebase/installations";
 
-// this is set during install
 let firebaseConfig;
+let firebaseApp;
+let auth;
+let installations;
 
 self.addEventListener("install", (event) => {
-  // extract firebase config from query string
   const serializedFirebaseConfig = new URL(location).searchParams.get(
     "firebaseConfig"
   );
@@ -18,6 +70,10 @@ self.addEventListener("install", (event) => {
   }
 
   firebaseConfig = JSON.parse(serializedFirebaseConfig);
+  firebaseApp = initializeApp(firebaseConfig);
+  auth = getAuth(firebaseApp);
+  installations = getInstallations(firebaseApp);
+
   console.log("Service worker installed with Firebase config", firebaseConfig);
 });
 
@@ -27,24 +83,28 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(fetchWithFirebaseHeaders(event.request));
 });
 
-// TODO: add Firebase Authentication headers to request
 async function fetchWithFirebaseHeaders(request) {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const installations = getInstallations(app);
   const headers = new Headers(request.headers);
   const [authIdToken, installationToken] = await Promise.all([
     getAuthIdToken(auth),
     getToken(installations),
   ]);
+
   headers.append("Firebase-Instance-ID-Token", installationToken);
   if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
+
   const newRequest = new Request(request, { headers });
-  return await fetch(newRequest);
+  return fetch(newRequest);
 }
 
 async function getAuthIdToken(auth) {
-  await auth.authStateReady();
-  if (!auth.currentUser) return;
-  return await getIdToken(auth.currentUser);
+  await new Promise((resolve) => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+
+  if (!auth.currentUser) return null;
+  return getIdToken(auth.currentUser);
 }
